b0f2b754fb70a82da5e8442f8b83f2577aca2221
gidskql6671 <gidskql6671@gmail.com>
2022-06-26 14:12:26 +0900

    popup

---
+++ public/background.js
@@ -0,0 +1,14 @@
+const color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  console.log(tab)
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content_script.js']
+  })
+})
